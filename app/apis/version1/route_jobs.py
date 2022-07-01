837a798144b96df06ac749f13d1ebc7b93efaef4
enricfranck <enricfranck@gmail.com>
2022-07-01 10:00:54 +0300

    test heroku

--- app/apis/version1/route_jobs.py
+++ app/apis/version1/route_jobs.py
@@ -20,14 +20,14 @@ router = APIRouter()
 
 
 @router.post("/create-job/", response_model=ShowJob)
-def create_job(job: JobCreate, db: Session = Depends(get_db)):
+def create_job(job: JobCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user_from_token)):
     current_user = 1
     job = create_new_job(job=job, db=db, owner_id=current_user)
     return job
 
 
 @router.get("/get/{id}", response_model=ShowJob)  # if we keep just "{id}" . it would stat catching all routes
-def read_job(id: int, db: Session = Depends(get_db)):
+def read_job(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user_from_token)):
     job = retreive_job(id=id, db=db)
     if not job:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with this id {id} does not exist")
@@ -35,13 +35,13 @@ def read_job(id: int, db: Session = Depends(get_db)):
 
 
 @router.get("/all", response_model=List[ShowJob])  # new
-def read_jobs(db: Session = Depends(get_db)):
+def read_jobs(db: Session = Depends(get_db), current_user: User = Depends(get_current_user_from_token)):
     jobs = list_jobs(db=db)
     return jobs
 
 
 @router.put("/update/{id}")  # new
-def update_job(id: int, job: JobCreate, db: Session = Depends(get_db)):
+def update_job(id: int, job: JobCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user_from_token)):
     current_user = 1
     message = update_job_by_id(id=id, job=job, db=db, owner_id=current_user)
     if not message:
